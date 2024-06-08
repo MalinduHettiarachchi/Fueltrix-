d535a03c91fc1af234c2d2befc7799bd18fac8ca
Malindu Hettiarachchi <mhssc20@gmail.com>
2024-06-08 16:02:26 +0530

    Create main web sides

---
+++ fueltrix/src/components/Navbar.js
@@ -0,0 +1,23 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">Fueltrix</Link>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/">Welcome</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
