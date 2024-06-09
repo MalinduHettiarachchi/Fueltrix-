dd9693134783fa93667755e43bf1083c10ca2c4c
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-06-09 19:58:52 +0530

    Add Footer

---
+++ fueltrix/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css'; // Import your CSS file for footer styles
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <p>&copy; {new Date().getFullYear()} Fueltrix. All rights reserved.</p>
+                <div className="social-icons">
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faFacebookF} />
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedinIn} />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
