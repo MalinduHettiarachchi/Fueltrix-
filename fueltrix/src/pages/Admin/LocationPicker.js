9d1e9708fe4fe8d18555c548ed91eab32deda4d7
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-10-01 18:51:37 +0530

    update

--- fueltrix/src/pages/Admin/LocationPicker.js
+++ fueltrix/src/pages/Admin/LocationPicker.js
@@ -1,10 +1,18 @@
 // LocationPicker.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-const LocationPicker = ({ setShedData, closeMap }) => {
+const LocationPicker = ({ setShedData, closeMap, shedLocation }) => {
   const [position, setPosition] = useState(null);
+
+  // Initialize the position to shedLocation if provided
+  useEffect(() => {
+    if (shedLocation) {
+      setPosition(shedLocation);
+    }
+  }, [shedLocation]);
 
   const LocationMarker = () => {
     useMapEvents({
@@ -18,21 +26,28 @@ const LocationPicker = ({ setShedData, closeMap }) => {
       },
     });
 
-    return position ? (
-      <Marker position={position}></Marker>
-    ) : null;
+    return position ? <Marker position={position} icon={highlightedIcon()} /> : null;
+  };
+
+  // Custom icon for highlighted shed location
+  const highlightedIcon = () => {
+    return L.icon({
+      iconUrl: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png', // Customize the icon as needed
+      iconSize: [38, 38], // size of the icon
+      iconAnchor: [22, 38], // point of the icon which will correspond to marker's location
+    });
   };
 
   return (
     <div style={{ height: '400px', width: '100%' }}>
       <MapContainer
-        center={[7.8731, 80.7718]} // Coordinates for Sri Lanka
-        zoom={8} // Adjust zoom level for better visibility of Sri Lanka
+        center={shedLocation || [7.8731, 80.7718]} // If no shed location, default to Sri Lanka
+        zoom={8} // Adjust zoom level
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-          url="https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
-          attribution='&copy; <a href="https://stamen.com">Stamen</a> contributors, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <LocationMarker />
       </MapContainer>
