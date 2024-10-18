bf323a48dd9c057461f7050e80481b05b57425f7
Malindu Hettiarachchi <mhssc20@gmail.com>
2024-10-19 00:52:00 +0530

    Update shed request

---
+++ fueltrix/src/pages/WebAdmin/ShedRequest/srequest.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../ShedRequest/srequest.css'
+
+function srequest() {
+  return (
+    <div className="shrc">
+      <div className="leftsr">
+      </div>
+      <div className="rightsr">
+      </div>
+    </div>
+  )
+}
+
+export default srequest
