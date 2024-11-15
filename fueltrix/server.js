c3f5d81367e72d6fccf2bfbdec0708318ae40c6e
Malindu Hettiarachchi <mhssc20@gmail.com>
2024-11-16 00:50:41 +0530

    Update

--- fueltrix/server.js
+++ fueltrix/server.js
@@ -7,8 +7,8 @@ const router = express.Router();
 const nodemailer = require("nodemailer");
 
 // Initialize Firestore with Firebase Admin SDK
-const serviceAccount = require("D:/NIBM/HND/Final Project/Project/fueltrix-b50cf-firebase-adminsdk-ww4uh-ecacdc9c1b.json");
-//Malidu(Ubee Path Eka)--E:/Projects/Fueltrix/fueltrix-b50cf-firebase-adminsdk-ww4uh-ecacdc9c1b.json
+const serviceAccount = require("D:/Project/Fueltrix/fueltrix-b50cf-firebase-adminsdk-ww4uh-ecacdc9c1b.json");
+//Malidu(Ubee Path Eka)--"D:/Project/Fueltrix/fueltrix-b50cf-firebase-adminsdk-ww4uh-ecacdc9c1b.json"
 //Mage path eka --D:/NIBM/HND/Final Project/Project/fueltrix-b50cf-firebase-adminsdk-ww4uh-ecacdc9c1b.json
 
 
